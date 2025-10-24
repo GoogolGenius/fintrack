d8c33a1981e41e89c07631525ef068492a4b6f4e
Abhay Suri <375881@student.linnmar.org>
2025-10-24 15:22:16 -0500

    add gitignore

--- scripts/index.js
+++ scripts/index.js
@@ -17,7 +17,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
 import { firebaseConfig } from "../config/firebase.js";
-import { firebaseConfig, openRouterApiKey } from "../config/firebase.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -1252,8 +1251,8 @@ if (openGoalModalBtn && goalDialog && goalForm) {
     });
 }
 
-const apiKey = openRouterApiKey;
-
+import { secrets } from "../config/secrets.js";
+const apiKey = secrets.openRouterKey;
 //Set FinBot's tone and give it rules.
 const systemMessage = {
   role: "system",
